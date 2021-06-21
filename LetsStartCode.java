50b526784858ad0a8aac110ec367d0d04e07d322
GezerM <82281175+GezerM@users.noreply.github.com>
2021-06-21 08:21:04 +0300

    Stop method is added to the the class

--- LetsStartCode.java
+++ LetsStartCode.java
@@ -6,7 +6,11 @@ public class Car{
   public void acceleration(){
     System.out.println(model + "'s 0-100 is 6.7 seconds");
   }
+  public void stop(){
+    System.out.println("Current velocity is zero");
+  }
   public static void main(String [] args){
     acceleration();
+    stop();
   }
     
