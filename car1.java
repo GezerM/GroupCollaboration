b77abddb25737f558d189636963296d0f84d54b9
ahmterby <erbay.1012@gmail.com>
2021-06-22 12:00:58 +0300

    b completed

--- car1.java
+++ car1.java
@@ -2,6 +2,7 @@ public class car1 {
     public static void main(String[] args) {
         System.out.println("branch creating");
         System.out.println("");
+        System.out.println("b branch completed");
 
     }
 }
